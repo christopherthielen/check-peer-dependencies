ebfb0d78cde4f78de3d19a72ba39c897cfa7859c
Chris Thielen <cthielen@netflix.com>
2026-01-07 14:53:15 -1000

    Format source files with prettier

--- src/solution.ts
+++ src/solution.ts
@@ -19,29 +19,34 @@ export interface Resolution {
   resolutionType: 'upgrade' | 'install' | 'devInstall';
 }
 
-export function findPossibleResolutions(problems: Dependency[], allPeerDependencies: Dependency[], ): Resolution[] {
-  const uniq: Dependency[] = problems.reduce((acc, problem) => acc.some(dep => dep.name === problem.name) ? acc : acc.concat(problem), []);
-  return uniq.map(problem => {
+export function findPossibleResolutions(problems: Dependency[], allPeerDependencies: Dependency[]): Resolution[] {
+  const uniq: Dependency[] = problems.reduce(
+    (acc, problem) => (acc.some((dep) => dep.name === problem.name) ? acc : acc.concat(problem)),
+    []
+  );
+  return uniq.map((problem) => {
     const shouldUpgrade = !!problem.installedVersion;
     const resolutionType = shouldUpgrade ? 'upgrade' : problem.isPeerDevDependency ? 'devInstall' : 'install';
     const resolutionVersion = findPossibleResolution(problem.name, allPeerDependencies);
     const resolution = resolutionVersion ? `${problem.name}@${resolutionVersion}` : null;
 
     return { problem, resolution, resolutionType } as Resolution;
-  })
+  });
 }
 
 function findPossibleResolution(packageName, allPeerDeps) {
-  const requiredPeerVersions = allPeerDeps.filter(dep => dep.name === packageName);
+  const requiredPeerVersions = allPeerDeps.filter((dep) => dep.name === packageName);
   // todo: skip this step if only one required peer version and it's an exact version
   const command = `npm view ${packageName} versions`;
   let rawVersionsInfo;
   try {
     rawVersionsInfo = exec(command, { silent: true }).stdout;
     const availableVersions = JSON.parse(rawVersionsInfo.replace(/'/g, '"')).sort(semverReverseSort);
-    return availableVersions.find(ver => requiredPeerVersions.every(peerVer => {
-      return semver.satisfies(ver, peerVer.version, { includePrerelease: true });
-    }));
+    return availableVersions.find((ver) =>
+      requiredPeerVersions.every((peerVer) => {
+        return semver.satisfies(ver, peerVer.version, { includePrerelease: true });
+      })
+    );
   } catch (err) {
     console.error(`Error while running command: '${command}'`);
     console.error(err);
